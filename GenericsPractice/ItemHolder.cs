e7dc1154288b4e41996f95d83eaf4a1523120104
minkaotic <mia.filisch@7digital.com>
2017-09-24 00:14:25 +0100

    Finish initial exercise

---
+++ GenericsPractice/ItemHolder.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace GenericsPractice
+{
+	public class ItemHolder<T>
+	{
+		private readonly T _item;
+
+		public ItemHolder(T item)
+		{
+			_item = item;
+		}
+
+		public T Get()
+		{
+			return _item;
+		}
+	}
+
+	internal class MultiItemHolder<T, T1>
+	{
+		private readonly T _item1;
+		private readonly T1 _item2;
+
+		public MultiItemHolder(T item1, T1 item2)
+		{
+			_item1 = item1;
+			_item2 = item2;
+		}
+
+		public T GetFirstItem()
+		{
+			return _item1;
+		}
+
+		public T1 GetSecondItem()
+		{
+			return _item2;
+		}
+	}
+}
---
+++ GenericsPractice/ItemHolderTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace GenericsPractice
+{
+	[TestFixture]
+	internal class ItemHolderTests
+	{
+		[Test]
+		public void I_can_get_an_item_of_type_string()
+		{
+			var sut = new ItemHolder<string>("randomString");
+
+			var getResult = sut.Get();
+
+			Assert.That(getResult, Is.TypeOf<string>());
+		}
+
+		[Test]
+		public void I_can_get_an_item_of_type_int()
+		{
+			var sut = new ItemHolder<int>(12);
+
+			var getResult = sut.Get();
+
+			Assert.That(getResult, Is.TypeOf<int>());
+		}
+
+		[Test]
+		public void I_can_get_an_item_of_custom_type()
+		{
+			var sut = new ItemHolder<Mias>(new Mias());
+
+			var mias = sut.Get();
+
+			Assert.That(mias, Is.TypeOf<Mias>());
+			Assert.That(mias.Cat, Is.EqualTo("Elsie"));
+			Assert.That(mias.Age, Is.EqualTo(34));
+		}
+
+		[Test]
+		public void I_can_get_multiple_items()
+		{
+			var sut = new MultiItemHolder<string, int>("hi", 100);
+
+			var item1 = sut.GetFirstItem();
+			var item2 = sut.GetSecondItem();
+
+			Assert.That(item1, Is.TypeOf<string>());
+			Assert.That(item2, Is.TypeOf<int>());
+
+		}
+
+		internal class Mias
+		{
+			public string Cat { get { return "Elsie"; } }
+			public int Age { get { return 34; } }
+		}
+	}
+}
